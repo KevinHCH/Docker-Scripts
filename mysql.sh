de145e8c8786a07fc17438aa219cca55a2b33f7e
Kevin <kevinherbas7@gmail.com>
2020-01-15 07:50:17 +0100

    Modificado script de creacion de MYSQL container para generar el volumen asociado y no perder datos

--- mysql.sh
+++ mysql.sh
@@ -1,29 +1,35 @@
-printf "## Deploying a MYSQL DATABASE ## \n"
+#!/bin/bash
+
+# This script will
+# -Create a volume to store your data between HOST and container
+# -Download the latest version of Mysql
+# -Add users and credentials
+
+printf "## We are going to create a Mysql Database (container) ## \n"
 sleep 0.5
-printf "Insert the password for your ROOT user: "
-read root_password
+read -p "Insert the name of the volume: " volume_name
+read -p "Insert the password for your ROOT user: " root_password
 
 printf "Do you want to create another user to manage this Database? (y/n)\n"
 read response
 
 if [ "$response" == "y" ]; then
-  printf "Insert the name for the new USER: "
-  read user_name
-  printf "Insert the password for this USER: "
-  read user_password
+  read -p "Insert the name for the new USER: " user_name
+  read -p "Insert the password for this USER: " user_password
 
   printf "We are creating the database...\n"
 
   sleep 1.5
-
-  docker_command="docker run --restart=always --name mysql -p 3306:3306 -e MYSQL_USER=$user_name -e MYSQL_PASSWORD=$user_password -e MYSQL_ROOT_PASSWORD=$root_password -d mysql:latest"
+  docker volume create $volume_name
+  docker_command="docker run --restart=always --name mysql -p 3306:3306 -e MYSQL_USER=$user_name -e MYSQL_PASSWORD=$user_password -e MYSQL_ROOT_PASSWORD=$root_password -v $volume_name:/usr/lib/mysql -d mysql:latest"
 else
-  docker_command="docker run --restart=always --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=$root_password -d mysql:latest"
+  docker_command="docker run --restart=always --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=$root_password -v $volume_name:/usr/lib/mysql -d mysql:latest"
 fi
-
+docker volume create $volume_name
 eval $docker_command
 
+sleep 0.5
 printf "The database has already created successfully.\n"
-sleep 1
+sleep 0.5
 
 exit
